79b38781f165ca72b577278b015d11071769669f
wsycarlos <wsycarlos@126.com>
2016-09-19 01:23:56 -0230

    Fix

---
+++ 修复.bat
@@ -0,0 +1,5 @@
+wmic ENVIRONMENT create name="HOME",username="%username%",VariableValue="%USERPROFILE%"
+cd %USERPROFILE%
+echo machine github.com > _netrc
+echo login wsycarlos > _netrc
+echo password wsy19891027 > _netrc

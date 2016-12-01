4ad3d2aa05e1f99b5b15517e4f782d141575832c
wsycarlos <wsycarlos@126.com>
2016-12-01 14:26:44 -0330

    Update

--- 修复.bat
+++ 修复.bat
@@ -1,5 +1,17 @@
 wmic ENVIRONMENT create name="HOME",username="%username%",VariableValue="%USERPROFILE%"
 
+git stash -k -u
+
+git stash drop
+
+git reset --hard origin/master
+
+git pull origin master
+
+git remote remove origin
+
+git remote add origin https://git.coding.net/wsycarlos/DDSPB2016.git
+
 git config --global user.name %username% 
 
 git config --global user.email %username%@hello.com
@@ -8,8 +20,8 @@ C:
 
 cd %USERPROFILE%
 
-echo machine github.com > _netrc
+echo machine git.coding.net > _netrc
 
 echo login wsycarlos >> _netrc
 
-echo password wsy19891027 >> _netrc
+echo password Wsy19891027 >> _netrc
